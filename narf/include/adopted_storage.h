d00a5a0007cb209760fe99f9e73b27a255c97e85
Josh Bendavid <Josh.Bendavid@cern.ch>
2022-01-31 01:21:55 +0000

    intermediate version

--- narf/include/adopted_storage.h
+++ narf/include/adopted_storage.h
@@ -14,17 +14,8 @@ namespace narf {
     using iterator = T*;
     using const_iterator = const T*;
 
-    // TODO current storage scheme minimizes the use of std::launder but leads to unnecessary default constructor calls in
-    // some cases when resizing.  Further optimizing this probably requires writing a dedicated iterator class.
-    
     adopted_storage(bool do_init, void *buffer, std::size_t buffer_size) :
-        do_init_(do_init),
-        buffer_size_(buffer_size),
-        buffer_(std::align(alignof(T), 0, buffer, buffer_size_)),
-        // TODO storing data_ pointer here is redundant, but maybe(?) needed for placement new in reset()
-        // using an intermediate array of (uninitialized) bytes should satisfy the requirements for implicit object creation
-        // of the array of T for the no-initialization case
-        data_(do_init_ ? new (buffer_) T[0]() : std::launder(reinterpret_cast<T*>(new (buffer_) std::byte[buffer_size_]))) {
+        do_init_(do_init) {
       // adopting memory in this way is only safe if the relevant classes have standard layout
       static_assert(std::is_standard_layout<T>::value);
       // the destructor will never be called
@@ -34,19 +25,40 @@ namespace narf {
       if (!do_init_ && !std::is_trivially_copy_constructible<T>::value) {
         throw std::runtime_error("cannot skip initialization for non-trivially copyable type");
       }
+      
+      void *aligned_buffer = std::align(alignof(T), 0, buffer, buffer_size);
+      capacity_ = buffer_size / sizeof(T);
+      if (do_init) {
+        // placement new implicitly creates array of T and produces a pointer to a suitable created object
+        // n.b. array elements do not begin their lifetime yet unless T is an implicit-lifetime type
+        data_ = static_cast<T*>(::operator new[](buffer_size, aligned_buffer));
+      }
+      else {
+        // memmove implicitly creates array of T and array elements with object representation from existing storage (and should be optimized out since source and destination are the same)
+        // n.b. std::launder for std::memmove source is needed to work around gcc bug https://gcc.gnu.org/bugzilla/show_bug.cgi?id=95349
+        // and ensures dynamic type is erased by std::memmove
+        // This should not be needed according to the standard (and clang behaves correctly without it)
+        // This would be a canonical use of std::start_lifetime_as if/when it is introduced to the standard
+        data_ = std::launder(static_cast<T*>(std::memmove(aligned_buffer, std::launder(static_cast<unsigned char*>(aligned_buffer)), buffer_size)));
+        // simpler minimal standard compliant version once gcc bug is fixed
+//         data_ = std::launder(static_cast<T*>(std::memmove(aligned_buffer, aligned_buffer, buffer_size)));
+      }
     }
 
     void reset(std::size_t n) {
-      if (n > max_size()) {
+      if (n > capacity_) {
         throw std::runtime_error("requested size too large");
       }
       if (do_init_) {
         if (n > initialized_size_) {
-          data_ = new (buffer_) T[n]();
+          std::fill_n(data_, initialized_size_, T{});
+          for (T *it = data_ + initialized_size_; it != data_ + n; ++it) {
+            new (it) T{};
+          }
           initialized_size_ = n;
         }
         else {
-          std::fill_n(data_, n, T());
+          std::fill_n(data_, n, T{});
         }
       }
       size_ = n;
@@ -80,16 +92,12 @@ namespace narf {
 
   private:
 
-    std::size_t max_size() const noexcept { return buffer_size_/sizeof(T); }
-
     bool do_init_;
 
     std::size_t size_{};
     std::size_t initialized_size_{};
 
-    std::size_t buffer_size_;
-    void *buffer_;
-    
+    std::size_t capacity_;
     T *data_;
   };
 
