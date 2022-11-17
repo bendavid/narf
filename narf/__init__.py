d0b92abf8c55ed46bc0b6025c2b790e79518d596
Josh Bendavid <Josh.Bendavid@cern.ch>
2022-11-17 19:42:57 +0000

    add narf version of fitter for testing

--- narf/__init__.py
+++ narf/__init__.py
@@ -7,5 +7,6 @@ from .graph_builder import build_and_run
 from .dataset import Dataset
 from .histutils import hist_to_root, root_to_hist, hist_to_pyroot_boost
 from .fitutils import fit_hist
+from .fitutilsjax import fit_hist_jax
 
-__all__ = ["build_and_run", "Dataset", "hist_to_root", "root_to_hist", "hist_to_pyroot_boost", "fit_hist"]
+__all__ = ["build_and_run", "Dataset", "hist_to_root", "root_to_hist", "hist_to_pyroot_boost", "fit_hist", "fit_hist_jax"]
