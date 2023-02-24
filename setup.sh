f9343cdb3381a3c5aa47f3bb0d066dbe746d68b6
Josh Bendavid <Josh.Bendavid@cern.ch>
2023-02-24 19:05:12 +0100

    add NARF_BASE environment variable and silence tensorflow warnings

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
-export PYTHONPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ):$PYTHONPATH"
+export NARF_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export PYTHONPATH="${NARF_BASE}:$PYTHONPATH"
+
+# silence tensorflow warnings
+# TODO check if this is still needed when tensorflow is updated to 2.11+
+export TF_CPP_MIN_LOG_LEVEL=3
+
+echo "Created environment variable NARF_BASE=${NARF_BASE}"
