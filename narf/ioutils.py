5ed9ba867acf60926a08154572dc30f57dfdec1d
Josh Bendavid <Josh.Bendavid@cern.ch>
2023-02-24 03:03:08 +0100

    refined hdf5 persistence implementation

--- narf/ioutils.py
+++ narf/ioutils.py
@@ -32,9 +32,13 @@ class H5PickleProxy:
         self.obj = None
 
 class H5Buffer():
-    def __init__(self, buf, h5dset = None):
+    def __init__(self, buf, h5dset = None, readonly = None):
         self.buf = buf
         self.h5dset = h5dset
+        self.readonly = readonly
+
+        if self.buf is not None:
+            self.readonly = memoryview(self.buf).readonly
 
     def checkdset(self):
         if self.h5dset is None:
@@ -45,19 +49,18 @@ class H5Buffer():
     def getbuffer(self):
         if self.buf is None:
             self.checkdset()
-            readonly = self.h5dset.attrs["readonly"]
             bufarr = np.empty_like(self.h5dset)
             if self.h5dset.size:
                 self.h5dset.read_direct(bufarr)
             bufout = bufarr.data
-            if readonly:
+            if self.readonly:
                 bufout = bufout.toreadonly()
             self.buf = bufout
 
         return self.buf
 
-    def readinto(self, buf):
-        bufarr = np.asarray(buf)
+    def readinto(self, b, /):
+        bufarr = np.asarray(b)
         if self.buf is None:
             self.checkdset()
             if self.h5dset.size:
@@ -112,50 +115,58 @@ def reduce_Hist(obj):
 
     return (make_Hist, (axes, obj.storage_type(), obj.metadata, obj.label, obj.name, h5buf) )
 
+class H5IO():
+    def __init__(self, name, h5out, mode = "r"):
+        if mode == "r":
+            self.dset = h5out[name]
+        elif mode == "w":
+            self.dset = h5out.create_dataset(name, shape=(0,), dtype = np.ubyte, maxshape=(None,), chunks = (1024*1024,), **hdf5plugin.Blosc(cname="lz4"))
+        else:
+            raise ValueError("Unsupported mode.")
+        self.pos = 0
+
+    def write(self, b, /):
+        if type(b) is bytes:
+            flat_view = memoryview(b)
+        else:
+            flat_view = pickle.PickleBuffer(b).raw()
+
+        oldsize = self.dset.size
+        nbytes = flat_view.nbytes
+        size = oldsize + nbytes
+        self.dset.resize((size,))
+        self.dset[oldsize:] = flat_view
+        return nbytes
+
+    def read(self, size=-1, /):
+        oldpos = self.pos
+        self.pos += size
+        return self.dset[oldpos:self.pos].data
+
+    def readline(self, size=-1, /):
+        raise NotImplementedError("readline is not implemented")
+
+    def readinto(self, b, /):
+        arr = np.frombuffer(b, dtype=np.ubyte)
+        nbytes = arr.nbytes
+        oldpos = self.pos
+        self.pos += nbytes
+        self.dset.read_direct(arr, source_sel = np.s_[oldpos:self.pos])
+        return nbytes
+
 class H5Pickler(pickle.Pickler):
     dispatch_table = copyreg.dispatch_table.copy()
     dispatch_table[bh.Histogram] = reduce_Histogram
     dispatch_table[hist.hist.Hist] = reduce_Hist
 
-    def __init__(self, protocol = 5):
-        self.current_group = None
-        self.io = io.BytesIO()
-        self.pickle_buffers = []
-        super().__init__(self.io, protocol=protocol, buffer_callback=self.pickle_buffers.append)
-
-    def dump(self, name, obj, h5group):
-        obj_group = h5group.create_group(name, track_order = True)
-        self.current_group = obj_group
-
-        try:
-            obj_group.attrs["narf_h5py_pickle_protocol_version"] = CURRENT_PROTOCOL_VERSION
-
-            self.io.truncate(0)
-            self.io.seek(0)
-            self.pickle_buffers.clear()
-
-            super().dump(obj)
-
-            dset = obj_group.create_dataset("pickle_data", data = self.io.getbuffer(), chunks = True, **hdf5plugin.Blosc(cname="lz4"), track_order = True)
-
-            bufgroup = obj_group.create_group("pickle_buffers", track_order = True)
-            for ibuf, buf in enumerate(self.pickle_buffers):
-                bufdset = bufgroup.create_dataset(f"pickle_buffer_{ibuf}", data = buf.raw(), chunks=True, **hdf5plugin.Blosc(cname="lz4"), track_order = True)
-                bufdset.attrs["readonly"] = buf.raw().readonly
-        except:
-            del obj_group
-            raise
-        finally:
-            self.current_group = None
-            self.io.truncate(0)
-            self.io.seek(0)
-            self.pickle_buffers.clear()
-
-        return obj_group
+    def __init__(self, h5group, protocol = 5):
+        self.h5group = h5group
+        self.io = H5IO("pickle_data", h5group, mode = "w")
+        super().__init__(self.io, protocol=protocol)
 
     def reduce_H5PickleProxy(self, proxy):
         PROXIED_OBJECTS_NAME = "proxied_objects"
-        current_location = self.current_group
+        current_location = self.h5group
 
         if PROXIED_OBJECTS_NAME in current_location:
             proxied_objects = current_location[PROXIED_OBJECTS_NAME]
@@ -165,11 +176,11 @@ class H5Pickler(pickle.Pickler):
         iproxy = len(proxied_objects)
         proxied_group = pickle_dump_h5py(f"proxied_object_{iproxy}", proxy.obj, proxied_objects)
 
-        return (type(proxy), (None, proxied_group,))
+        return (type(proxy), (None, proxied_group))
 
     def reduce_H5Buffer(self, h5buf):
         H5BUFFER_GROUP_NAME = "h5_buffers"
-        current_location = self.current_group
+        current_location = self.h5group
 
         if H5BUFFER_GROUP_NAME in current_location:
             h5_buffers = current_location[H5BUFFER_GROUP_NAME]
@@ -177,17 +188,10 @@ class H5Pickler(pickle.Pickler):
             h5_buffers = current_location.create_group(H5BUFFER_GROUP_NAME, track_order = True)
 
         ibuf = len(h5_buffers)
-        buf = pickle.PickleBuffer(h5buf.getbuffer())
-        bufdset = h5_buffers.create_dataset(f"h5_buffer_{ibuf}", data = buf.raw(), chunks=True, **hdf5plugin.Blosc(cname="lz4"), track_order = True)
-        bufdset.attrs["readonly"] = buf.raw().readonly
+        flat_view = pickle.PickleBuffer(h5buf.getbuffer()).raw()
+        bufdset = h5_buffers.create_dataset(f"h5_buffer_{ibuf}", data = flat_view, chunks=True, **hdf5plugin.Blosc(cname="lz4"))
 
-        return (type(h5buf), (None, bufdset,))
-
-    def persistent_id(self, obj):
-        if isinstance(obj, h5py.Group) or isinstance(obj, h5py.Dataset):
-            return H5Path(obj.name)
-        else:
-            return None
+        return (type(h5buf), (None, bufdset, flat_view.readonly))
 
     def reducer_override(self, obj):
         if type(obj) is H5PickleProxy:
@@ -197,70 +201,45 @@ class H5Pickler(pickle.Pickler):
         else:
             return NotImplemented
 
+    def persistent_id(self, obj):
+        if isinstance(obj, h5py.Group) or isinstance(obj, h5py.Dataset):
+            return H5Path(obj.name)
+        else:
+            return None
+
 class H5Unpickler(pickle.Unpickler):
-    def __init__(self):
-        self.current_group = None
-        self.io = io.BytesIO()
-        self.pickle_buffers = []
-        super().__init__(self.io, buffers = self.pickle_buffers)
-    def load(self, h5group):
-        try:
-            narf_protocol_version = h5group.attrs["narf_h5py_pickle_protocol_version"]
-        except KeyError:
-            raise ValueError("h5py dataset does not contain a python object pickled by narf.")
-
-        if narf_protocol_version < MIN_PROTOCOL_VERSION:
-            raise ValueError(f"Unsuppported narf protocol version {narf_protocol_version}, minimum supported version is {MIN_PROTOCOL_VERSION}")
-        elif narf_protocol_version > CURRENT_PROTOCOL_VERSION:
-            raise ValueError(f"Unsuppported narf protocol version {narf_protocol_version}, maximum supported version is {current_protocol_version}")
-
-        self.current_group = h5group
-
-        try:
-            dset = h5group["pickle_data"]
-
-            self.io.truncate(0)
-            self.io.seek(0)
-            self.pickle_buffers.clear()
-
-            arr = np.empty_like(dset)
-            if dset.size:
-                dset.read_direct(arr)
-
-            self.io.write(arr)
-            self.io.seek(0)
-
-            bufgroup = h5group["pickle_buffers"]
-            for bufdset in bufgroup.values():
-                readonly = bufdset.attrs["readonly"]
-                bufarr = np.empty_like(bufdset)
-                if bufdset.size:
-                    bufdset.read_direct(bufarr)
-                bufout = bufarr.data
-                if readonly:
-                    bufout = bufout.toreadonly()
-                self.pickle_buffers.append(bufout)
-
-            obj = super().load()
-
-        finally:
-            self.current_group = None
-            self.io.truncate(0)
-            self.io.seek(0)
-            self.pickle_buffers.clear()
-
-        return obj
+    def __init__(self, h5group):
+        self.h5group = h5group
+        self.io = H5IO("pickle_data", h5group, mode = "r")
+        super().__init__(self.io)
 
     def persistent_load(self, pid):
-        if isinstance(pid, H5Path):
-            return self.current_group.file[pid.path]
+        if type(pid) is H5Path:
+            return self.h5group.file[pid.path]
         else:
             raise pickle.UnpicklingError("unsupported persistent object")
 
 def pickle_dump_h5py(name, obj, h5out):
-    pickler = H5Pickler()
-    return pickler.dump(name, obj, h5out)
+    obj_group = h5out.create_group(name)
+    try:
+        obj_group.attrs["narf_h5py_pickle_protocol_version"] = CURRENT_PROTOCOL_VERSION
+        pickler = H5Pickler(obj_group)
+        pickler.dump(obj)
+    except:
+        del obj_group
+        raise
+    return obj_group
 
 def pickle_load_h5py(h5group):
-    unpickler = H5Unpickler()
-    return unpickler.load(h5group)
+    try:
+        narf_protocol_version = h5group.attrs["narf_h5py_pickle_protocol_version"]
+    except KeyError:
+        raise ValueError("h5py dataset does not contain a python object pickled by narf.")
+
+    if narf_protocol_version < MIN_PROTOCOL_VERSION:
+        raise ValueError(f"Unsuppported narf protocol version {narf_protocol_version}, minimum supported version is {MIN_PROTOCOL_VERSION}")
+    elif narf_protocol_version > CURRENT_PROTOCOL_VERSION:
+        raise ValueError(f"Unsuppported narf protocol version {narf_protocol_version}, maximum supported version is {current_protocol_version}")
+
+    unpickler = H5Unpickler(h5group)
+    return unpickler.load()
