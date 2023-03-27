56185f1fe35e6cdf97ea26d428e6b1251bc1b3db
Josh Bendavid <Josh.Bendavid@cern.ch>
2023-03-27 22:14:09 +0000

    set EXTRA_CLING_ARGS in setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 export NARF_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export PYTHONPATH="${NARF_BASE}:$PYTHONPATH"
+export EXTRA_CLING_ARGS="-O3"
 
 echo "Created environment variable NARF_BASE=${NARF_BASE}"
