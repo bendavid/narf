9314e90117d3bc9d7c5c3c8fa985f6b9f5facc51
Josh Bendavid <Josh.Bendavid@cern.ch>
2023-09-06 19:50:09 +0000

    additional environment variable needed to protect against bad threading behaviour in some tensorflow builds

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ export TF_NUM_INTEROP_THREADS="1"
 
 #workaround for lock contention issue in tflite
 export TF_ENABLE_ONEDNN_OPTS="0"
+export TF_DISABLE_MKL="1"
 
 echo "Created environment variable NARF_BASE=${NARF_BASE}"
