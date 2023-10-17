92dc9ac2d60f30982683660c10b15933291b97d4
Josh Bendavid <Josh.Bendavid@cern.ch>
2023-10-17 12:32:56 -0400

    set env variable to improve xrootd performance for multithreaded applications

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 export NARF_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export PYTHONPATH="${NARF_BASE}:$PYTHONPATH"
 export EXTRA_CLING_ARGS="-O3"
+export XRD_PARALLELEVTLOOP="16"
 
 #workaround for bug in tf lite
 export TF_NUM_INTEROP_THREADS="1"
