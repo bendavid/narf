f79a9eac676826ab47eb0d31edb3445c1d189ef3
Josh Bendavid <Josh.Bendavid@cern.ch>
2024-09-28 17:30:26 +0000

    add wrapper function for cling library loading

--- narf/clingutils.py
+++ narf/clingutils.py
@@ -4,4 +4,10 @@ def Declare(code):
     ret = ROOT.gInterpreter.Declare(code)
 
     if not ret:
-        raise ValueError("Call to gInterpreter.Declare failed")
+        raise ValueError(f"Call to gInterpreter.Declare failed")
+
+def Load(filename):
+    ret = ROOT.gInterpreter.Load(filename)
+
+    if ret != 0:
+        raise ValueError(f"Call to gInterpreter.Load failed")
