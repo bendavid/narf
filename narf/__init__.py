7382a7ee1775c2d0078295a7e5d7f91b5f9ba644
davidwalter2 <david.walter@cern.ch>
2025-03-05 03:59:11 -0500

    Remove fitutils, fitutilsjax, tfutils (now in wums)

--- narf/__init__.py
+++ narf/__init__.py
@@ -7,5 +7,4 @@ from .graph_builder import build_and_run
 from .dataset import Dataset
 from .histutils import hist_to_root, root_to_hist, hist_to_pyroot_boost
 
-import narf.ioutils
 import narf.clingutils
