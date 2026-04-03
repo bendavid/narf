c660d394373a794003d841b0f96e1042f79d2826
Josh Bendavid <Josh.Bendavid@cern.ch>
2026-04-03 10:12:40 -0400

    remove debug printouts

--- narf/rdfutils.py
+++ narf/rdfutils.py
@@ -6,8 +6,6 @@ narf.clingutils.Declare('#include "progresshelper.hpp"')
 
 def flexible_graph_operation(df, name, graph_function, func, cols):
     coltypes = [df.GetColumnType(col) for col in cols]
-    print(func)
-    print(coltypes)
     wrappedhelper = ROOT.narf.DefineWrapper[type(func), *coltypes](func)
     return graph_function(name, wrappedhelper, cols)
 
