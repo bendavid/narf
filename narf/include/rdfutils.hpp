db0394fb1a70096feec46ac3d7f5620f69bc31a5
Josh Bendavid <Josh.Bendavid@cern.ch>
2026-04-05 00:00:27 -0400

    make wrapper more flexible/robust

--- narf/include/rdfutils.hpp
+++ narf/include/rdfutils.hpp
@@ -8,14 +8,14 @@ namespace narf {
     private:
         Callable callable_;
 
-        using return_t = decltype(callable_(std::declval<Args>()...));
+        using return_t = std::decay_t<decltype(callable_(std::declval<Args>()...))>;
 
     public:
 
         DefineWrapper(const Callable &callable) : callable_(callable) {}
         DefineWrapper(Callable &&callable) : callable_(std::move(callable)) {}
 
-        return_t operator() (const Args&... args) const {
+        return_t operator() (const Args&... args) {
             return callable_(args...);
         }
     };
