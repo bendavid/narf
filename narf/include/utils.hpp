ff899e4a20d006911018d2639ecf4b8d33dd7ce5
Josh Bendavid <Josh.Bendavid@cern.ch>
2026-04-06 05:57:05 -0400

    make range_to more flexible

--- narf/include/utils.hpp
+++ narf/include/utils.hpp
@@ -136,8 +136,8 @@ namespace narf {
             | std::views::transform([](auto const &it){ return *it; });
   }
 
-  template <template <typename> class C, class T>
-  auto range_to(T &range) {
+  template <template <typename> class C=ROOT::VecOps::RVec, class T>
+  auto range_to(T &&range) {
     using value_type = std::decay_t<decltype(*range.begin())>;
 
     C<value_type> res;
